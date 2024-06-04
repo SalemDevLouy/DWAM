07611aadd29e3e955686d04dae53e17030f98a36
Salem Louy <98494370+simo0loufi@users.noreply.github.com>
2024-06-05 01:43:08 +0200

    first commit DWAM Landing Page
    first commit DWAM Landing Page

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+const config: Config = {
+  mode: "jit",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        trueGray: colors.neutral,
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      stock: [defaultTheme.fontFamily.sans],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
