b6fc4367043f9b0bbfc2a6b1d0d7951af04e489d
Salem Louy <98494370+simo0loufi@users.noreply.github.com>
2024-06-05 11:03:36 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,8 +33,6 @@ export function Footer() {
               startups and indie projects. Its built with Next.js & TailwindCSS.
               And its completely open-source.
             </div>
-
-    
           </div>
 
           <div>
@@ -99,13 +97,19 @@ export function Footer() {
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made  by{" SALEM LOUAFI ♥ with "}
+          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
           <a href="https://web3templates.com/" target="_blank" rel="noopener">
             Web3Templates.
           </a>{" "}
+          Illustrations from{" "}
+          <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
+            Glazestock
+          </a>
+          ,Modified by Louafi Salem
         </div>
       </Container>
       {/* Do not remove this */}
+      <Backlink />
     </div>
   );
 }
@@ -157,3 +161,30 @@ const Linkedin = ({ size = 24 }) => (
   </svg>
 );
 
+const Backlink = () => {
+  return (
+    <a
+      href="https://web3templates.com"
+      target="_blank"
+      rel="noopener"
+      className="absolute flex px-3 py-1 space-x-2 text-sm font-semibold text-gray-900 bg-white border border-gray-300 rounded shadow-sm place-items-center left-5 bottom-5 dark:bg-trueGray-900 dark:border-trueGray-700 dark:text-trueGray-300"
+    >
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 30 30"
+        fill="none"
+        className="w-4 h-4"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="30" height="29.5385" rx="2.76923" fill="#362F78" />
+        <path
+          d="M10.14 21.94H12.24L15.44 12.18L18.64 21.94H20.74L24.88 8H22.64L19.58 18.68L16.36 8.78H14.52L11.32 18.68L8.24 8H6L10.14 21.94Z"
+          fill="#F7FAFC"
+        />
+      </svg>
+
+      <span>Web3Templates</span>
+    </a>
+  );
+};
